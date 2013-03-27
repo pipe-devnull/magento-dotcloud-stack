c184dadeb565b71da24afeec40928f805ef8e5c4
Adrian Bruce <ade@pipe-devnull.com>
2013-03-27 20:32:11 +0000

    Fix installer script, skip url validation

--- README.md
+++ README.md
@@ -1,4 +1,30 @@
 magento-dotcloud-stack
 ======================
 
-Install Magento on dotcloud instance 
+Install Magento on dotcloud instance -  just follow the standard dotclound install procedure.....
+
+
+    git clone https://github.com/pipe-devnull/magento-dotcloud-stack.git
+    cd magento-dotcloud-stack
+    dotcloud create mage
+    dotcloud push
+
+
+
+Default values:
+=====================
+
+The Magento CLI installer is ran with a set of default values listed below
+
+
+Admin username - admin
+Admin password - test123
+locale		   - en_GB
+timezone       - Europe/London
+currency       - GBP
+
+
+Todo
+===================
+
+* Optionally install sample data
--- mage-install.php
+++ mage-install.php
@@ -2,7 +2,7 @@
 <?php
 /**
  * Postinstall script for magento on dotcloud
- *
+ * 
  */
 
 echo "Post Install script started \n";
@@ -13,80 +13,79 @@ $db_name = 'magento';
 
 if (file_exists($environment_variables_path))
 {
-	$environment_variables = json_decode(file_get_contents("/home/dotcloud/environment.json"));
-	echo "dotcloud environment variables file found & decoded \n";
+    $environment_variables = json_decode(file_get_contents("/home/dotcloud/environment.json"));
+    echo "dotcloud environment variables file found & decoded \n";
 }
 
 
 /**
  * Create empty magento database ready for new install
  * 
- * @return void
- * 
  */
 function create_database($environment_variables, $db_name)
 {
-	echo $environment_variables->DOTCLOUD_DB_MYSQL_HOST;
-	$dsn = "mysql:host=" . $environment_variables->DOTCLOUD_DB_MYSQL_HOST . ";port=" . $environment_variables->DOTCLOUD_DB_MYSQL_PORT;
+    echo "Host: " . $environment_variables->DOTCLOUD_DB_MYSQL_HOST . "\n";
 
-    $password = $environment_variables->DOTCLOUD_DB_MYSQL_PASSWORD;
-    $user 	  = $environment_variables->DOTCLOUD_DB_MYSQL_LOGIN;
+    $host       = $environment_variables->DOTCLOUD_DB_MYSQL_HOST;
+    $password   = $environment_variables->DOTCLOUD_DB_MYSQL_PASSWORD;
+    $user       = $environment_variables->DOTCLOUD_DB_MYSQL_LOGIN;
+    $port       = $environment_variables->DOTCLOUD_DB_MYSQL_PORT;
+
+    $dsn = "mysql:host=" . $host . ";port=" . $port;
 
     try
-    {	
-    	$dbh = new PDO($dsn, $user, $password);
-    	$dbh->exec("CREATE DATABASE IF NOT EXISTS " . $db_name) or die("Error creating DB");
+    {   
+        $dbh = new PDO($dsn, $user, $password);
+        $dbh->exec("CREATE DATABASE IF NOT EXISTS " . $db_name) or die("PDO Error creating DB");
     } 
     catch (Exception $e)
     {
-    	echo "Error creating DB: " . $e->getMessage();
+        echo "Error creating DB: " . $e->getMessage();
     }
 }
 
 /**
  * Run magento PHP CLI installer
  * 
- * @param  array $environment_variables dotcloud environment variables
- * 
- * @return void
  */
 function run_install($environment_variables, $doc_root)
 {
-	$options_list = array(
-				"--license_agreement_accepted" => "yes",
-            	"--locale" => "en_GB",
-            	"--timezone" => "Europe/London",
-            	"--default_currency" => "GBP",
-            	"--db_host" => $environment_variables->DOTCLOUD_DB_MYSQL_HOST,
-            	"--db_name" => 'magento',
-            	"--db_user" => $environment_variables->DOTCLOUD_DB_MYSQL_LOGIN,
-            	"--db_pass" => $environment_variables->DOTCLOUD_DB_MYSQL_PASSWORD,
-            	"--session_save" => "db",
-            	"--admin_frontname" => "system",
-            	"--url" => $environment_variables->DOTCLOUD_WWW_HTTP_URL,
-            	"--use_rewrites" => "no", # leave rewrites off for now
-            	"--use_secure" => "yes",
-            	"--secure_base_url" => $environment_variables->DOTCLOUD_WWW_HTTP_URL,
-            	"--use_secure_admin" => "yes",
-            	"--admin_firstname" => "admin",
-            	"--admin_lastname" => "admin",
-            	"--admin_email" => $environment_variables->DOTCLOUD_EMAIL,
-            	"--admin_username" => "admin",
-            	"--admin_password" => "test123",
-            	"--allow_symlink" => "1",
-            	"--encryption_key" => "BRuvuCrUd4aSWutr"
-        	);
+    $baseUrl = rtrim($environment_variables->DOTCLOUD_WWW_HTTP_URL, '/');
 
-	$options = "";
-	foreach ($options_list as $option => $value)
-	{
-		$options = $options . " " . $option . " " . "'" . $value . "'";
-	}
-	
-	$cmd = "php -f " . $doc_root . "install.php -- " . $options;
-	system($cmd);
+    $options_list = array(
+                "--license_agreement_accepted" => "yes",
+                "--skip_url_validation"        => "yes",
+                "--locale"                     => "en_GB",
+                "--timezone"                   => "Europe/London",
+                "--default_currency"           => "GBP",
+                "--db_host"                    => $environment_variables->DOTCLOUD_DB_MYSQL_HOST . ":" . $environment_variables->DOTCLOUD_DB_MYSQL_PORT,
+                "--db_name"                    => 'magento',
+                "--db_user"                    => $environment_variables->DOTCLOUD_DB_MYSQL_LOGIN,
+                "--db_pass"                    => $environment_variables->DOTCLOUD_DB_MYSQL_PASSWORD,
+                "--session_save"               => "db",
+                "--admin_frontname"            => "system",
+                "--url"                        => $baseUrl,
+                "--use_rewrites"               => "no", # leave rewrites off for now
+                "--use_secure"                 => "yes",
+                "--secure_base_url"            => $baseUrl,
+                "--use_secure_admin"           => "yes",
+                "--admin_firstname"            => "admin",
+                "--admin_lastname"             => "admin",
+                "--admin_email"                => $environment_variables->DOTCLOUD_EMAIL,
+                "--admin_username"             => "admin",
+                "--admin_password"             => "test123",
+                "--allow_symlink"              => "1",
+            );
 
-	echo "Magento CLI installer complete \n ";
+    $options = "";
+    foreach ($options_list as $option => $value)
+    {
+        $options = $options . " " . $option . " " . "'" . $value . "'";
+    }
+    
+    $cmd = "php -f " . $doc_root . "install.php -- " . $options;
+
+    system($cmd);
 }
 
 echo "Creating database \n";
